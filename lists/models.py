64d34566ea99abf4fea2783af671f5051e36e7cb
Danial Gagne <gagne.danial@gmail.com>
2017-10-07 09:31:33 -0500

    changed model to allow items associated to different lists

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
+## to clear db:
+## rm db.sqlite3
+## python manage.py migrate --noinput
+	
+class List (models.Model):
+	pass
+	
 class Item(models.Model):
 	text = models.TextField(default='')
+	list = models.ForeignKey(List, default=None)
