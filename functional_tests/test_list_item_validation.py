a760119fc356287b0b1fc527308b651216acba0c
Danial Gagne <gagne.danial@gmail.com>
2017-10-31 22:41:41 -0500

    finished validation FT and created UT folder with UT file

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.keys import Keys
+
 from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
@@ -5,15 +7,32 @@ class ItemValidationTest(FunctionalTest):
 	def test_cannot_add_empty_list_items(self):
 		# I go to the home page and accidentally try to submit
 		# an empty list item. I hit Enter on the empty input box
-
+		self.browser.get(self.live_server_url)
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+		
 		# The home page refreshes, and there is an error message saying
 		# that list items cannot be blank
+		self.wait_for(lambda: self.assertEqual(
+			self.browser.find_element_by_css_selector('.has_error').text,
+			"You can't have an empty list item"
+		))
 
 		# I try again with some text for the item, which now works
-
+		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
+		
 		# Perversely, I now decide to submit a second blank list item
-
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+		
 		# I receive a similar warning on the list page
+		self.wait_for(lambda: self.assertEqual(
+			self.browser.find_element_by_css_selector('.has_error').text,
+			"You can't have an empty list item"
+		))
 
 		# And I can correct it by filling some text in
-		self.fail('write me!')
+		self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
+		self.wait_for_row_in_list_table('2: Make tea')
