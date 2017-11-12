7189e8025f29253ac326fb2caafebb73a535f0b4
Danial Gagne <gagne.danial@gmail.com>
2017-11-12 10:29:02 -0600

    completed form validation in views and models for duplicate items. tests pass

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,10 +1,9 @@
 from django.test import TestCase
 from django.utils.html import escape
-from unittest import skip
 
 from lists.models import Item, List
 from lists.forms import (
-	 EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
+	 EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR,
 	 ItemForm, ExistingListItemForm
 )
 
@@ -75,7 +74,7 @@ class ListViewTest(TestCase):
 	def test_displays_item_form(self):
 		list_ = List.objects.create()
 		response = self.client.get(f'/lists/{list_.id}/')
-		self.assertIsInstance(response.context['form'], ItemForm)
+		self.assertIsInstance(response.context['form'], ExistingListItemForm)
 		self.assertContains(response, 'name="text"')
 		
 	def post_invalid_input(self):
@@ -96,13 +95,12 @@ class ListViewTest(TestCase):
 
 	def test_for_invalid_input_passes_form_to_template(self):
 		response = self.post_invalid_input()
-		self.assertIsInstance(response.context['form'], ItemForm)
+		self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
 	def test_for_invalid_input_shows_error_on_page(self):
 		response = self.post_invalid_input()
 		self.assertContains(response, escape(EMPTY_ITEM_ERROR))
-	
-	@skip	
+		
 	def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
 		list1 = List.objects.create()
 		item1 = Item.objects.create(list=list1, text='some text')
@@ -110,10 +108,10 @@ class ListViewTest(TestCase):
 			f'/lists/{list1.id}/',
 			data={'text': 'some text'}
 		)
-		expected_error = escape("You've already got this in your list")
+		expected_error = escape(DUPLICATE_ITEM_ERROR)
 		self.assertContains(response, expected_error)
 		self.assertTemplateUsed(response, 'list.html')
-		self.AssertEqual(Item.objects.all().count(), 1)
+		self.assertEqual(Item.objects.all().count(), 1)
 		
 				
 class NewListTest(TestCase):
