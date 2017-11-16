53ef44230fda3938bbb128e938a5fbe0fc762715
Danial Gagne <gagne.danial@gmail.com>
2017-11-15 23:01:20 -0600

    created FT for clearing duplicate item error message. test does not pass

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,6 +4,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 	
+	def get_error_element(self):
+		return self.browser.find_element_by_css_selector('.has-error')
+	
 	def test_cannot_add_empty_list_items(self):
 		# I go to the home page and accidentally try to submit
 		# an empty list item. I hit Enter on the empty input box
@@ -56,6 +59,27 @@ class ItemValidationTest(FunctionalTest):
 		
 		# I see a helpful error message
 		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text,
+			self.get_error_element().text,
 			"You've already got this in your list"
+		))
+		
+	def test_error_messages_are_cleared_on_input(self):
+		# I start a list and cause a validation error
+		self.browser.get(self.live_server_url)
+		self.get_item_input_box().send_keys('read book')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: read book')
+		self.get_item_input_box().send_keys('read book')
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		
+		self.wait_for(lambda: self.assertTrue(
+			self.get_error_element().is_displayed()
+		))
+		
+		# I start typing in the input box to clear the error
+		self.get_item_input_box().send_keys('a')
+		
+		# the error message disappears
+		self.wait_for(lambda: self.assertFalse(
+			self.get_error_element().is_displayed()
 		))
